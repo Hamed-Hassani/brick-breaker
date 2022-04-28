407dea036c8d4fddcf376c73808e558fc1b0553e
HamedHassani <hamed2008ir@gmail.com>
2022-04-28 13:57:21 +0430

    upload game 2d

---
+++ Assets/Scripts/BrickScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BrickScript : MonoBehaviour
+{
+    public UiManager ui;
+    void Start()
+    {
+        ui = GameObject.FindWithTag("ui").GetComponent<UiManager>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            ui.IncrementScore();
+            Destroy(gameObject);
+        }
+    }
+
+
+}
